80cfc78d11849940ad57e2e428ec6b21c42607f8
friedooo <friedlich3@gmail.com>
2021-11-05 00:38:45 +0300

    feat: implement any flag order and args length check

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-console.log(process.argv);
+const { stdin, stdout, stderr } = process;
 
-// if (process.argv[2] != 1) {
-//   process.stderr();
-// }
+let args = process.argv;
 
-// node index -c "C1-C1-R0-A" -i "./input.txt" -o "./output.txt
+const checkArgsLength = (arr) => {
+  if (arr.length === 8) {
+    console.log("количество аргументов сходится");
+  }
+};
+
+const getDataObj = (arr) => {
+  let dataObj = {
+    algo: "",
+    inputFile: "",
+    outputFile: "",
+  };
+
+  arr.forEach((elem, i) => {
+    if (elem === "-c" || elem === "--config") {
+      dataObj.algo = arr[i + 1];
+    } else if (elem === "-i" || elem === "--input") {
+      dataObj.inputFile = arr[i + 1];
+    } else if (elem === "-o" || elem === "--output") {
+      dataObj.outputFile = arr[i + 1];
+    }
+  });
+
+  return dataObj;
+};
+
+checkArgsLength(args);
+console.log(getDataObj(args));
+
+// console.log(process.stderr.write("что-то пошло не так"));
+
+// node index -c "C1-C1-R0-A" -i "./input.txt" -o "./output.txt"
+
+// stdout.write("Как тебя зовут?\n");
+// stdin.on("data", (data) => {
+//   stdout.write("Привет, ");
+//   stdout.write(data);
+//   process.exit();
+// });
+// process.on("exit", () => stdout.write("Удачи!"));
