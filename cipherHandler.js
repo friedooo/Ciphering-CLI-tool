9d1178d15b894d279860ae9ed12ee30e593ce139
friedooo <friedlich3@gmail.com>
2021-11-07 01:19:53 +0300

    feat: implement Atbash encoder

--- cipherHandler.js
+++ cipherHandler.js
@@ -1,30 +1,30 @@
-module.exports.RotEncoder = (str, shift = 0) => {
+module.exports.rotEncoder = (str, shift = 0) => {
   const arr = str.split("");
   const lettersAmount = 26;
   const realShift = Math.abs(shift % lettersAmount);
-  const lowercaseBorder = [65, 90];
-  const uppercaseBorder = [97, 122];
+  const upperCaseBorder = [65, 90];
+  const lowerCaseBorder = [97, 122];
 
   if (shift > -1) {
     return arr
       .map((elem) => {
         let elemCode = elem.charCodeAt();
         if (
-          elemCode > lowercaseBorder[0] - 1 &&
-          elemCode < lowercaseBorder[1] + 1
+          elemCode > upperCaseBorder[0] - 1 &&
+          elemCode < upperCaseBorder[1] + 1
         ) {
           elemCode += realShift;
 
-          if (elemCode > lowercaseBorder[1]) {
+          if (elemCode > upperCaseBorder[1]) {
             elemCode = elemCode - lettersAmount;
           }
         } else if (
-          elemCode > uppercaseBorder[0] - 1 &&
-          elemCode < uppercaseBorder[1] + 1
+          elemCode > lowerCaseBorder[0] - 1 &&
+          elemCode < lowerCaseBorder[1] + 1
         ) {
           elemCode += realShift;
 
-          if (elemCode > uppercaseBorder[1]) {
+          if (elemCode > lowerCaseBorder[1]) {
             elemCode = elemCode - lettersAmount;
           }
         }
@@ -38,21 +38,21 @@ module.exports.RotEncoder = (str, shift = 0) => {
       .map((elem) => {
         let elemCode = elem.charCodeAt();
         if (
-          elemCode > lowercaseBorder[0] - 1 &&
-          elemCode < lowercaseBorder[1] + 1
+          elemCode > upperCaseBorder[0] - 1 &&
+          elemCode < upperCaseBorder[1] + 1
         ) {
           elemCode -= realShift;
 
-          if (elemCode < lowercaseBorder[0]) {
+          if (elemCode < upperCaseBorder[0]) {
             elemCode = elemCode + lettersAmount;
           }
         } else if (
-          elemCode > uppercaseBorder[0] - 1 &&
-          elemCode < uppercaseBorder[1] + 1
+          elemCode > lowerCaseBorder[0] - 1 &&
+          elemCode < lowerCaseBorder[1] + 1
         ) {
           elemCode -= realShift;
 
-          if (elemCode < uppercaseBorder[0]) {
+          if (elemCode < lowerCaseBorder[0]) {
             elemCode = elemCode + lettersAmount;
           }
         }
@@ -60,4 +60,47 @@ module.exports.RotEncoder = (str, shift = 0) => {
       })
       .join("");
   }
+};
+
+module.exports.atbashEncoder = (str) => {
+  const arr = str.split("");
+  const lettersAmount = 26;
+  const upperCaseBorder = [65, 90];
+  const lowerCaseBorder = [97, 122];
+
+  return arr
+    .map((elem) => {
+      let elemCode = elem.charCodeAt();
+
+      if (
+        elemCode > upperCaseBorder[0] - 1 &&
+        elemCode < upperCaseBorder[1] + 1
+      ) {
+        let dif = Math.abs(
+          Math.abs(elemCode - upperCaseBorder[0]) * 2 - lettersAmount + 1
+        );
+
+        if (elemCode - upperCaseBorder[0] < upperCaseBorder[1] - elemCode) {
+          elemCode += dif;
+        } else {
+          elemCode -= dif;
+        }
+      } else if (
+        elemCode > lowerCaseBorder[0] - 1 &&
+        elemCode < lowerCaseBorder[1] + 1
+      ) {
+        let dif = Math.abs(
+          Math.abs(elemCode - lowerCaseBorder[0]) * 2 - lettersAmount + 1
+        );
+
+        if (elemCode - lowerCaseBorder[0] < lowerCaseBorder[1] - elemCode) {
+          elemCode += dif;
+        } else {
+          elemCode -= dif;
+        }
+      }
+
+      return String.fromCharCode(elemCode);
+    })
+    .join("");
 };
--- index.js
+++ index.js
@@ -31,50 +31,70 @@
 // checkArgsLength(args);
 // console.log(getDataObj(args));
 
-const { RotEncoder } = require("./cipherHandler");
+const { rotEncoder, atbashEncoder } = require("./cipherHandler");
+
 console.log("encoding rot test");
 console.log(
-  RotEncoder(`This is secret. Message about "_" symbol!`, 1) ===
+  rotEncoder(`This is secret. Message about "_" symbol!`, 1) ===
     'Uijt jt tfdsfu. Nfttbhf bcpvu "_" tzncpm!'
 );
 console.log(
-  RotEncoder(`This is secret. Message about "_" symbol!`, 4) ===
+  rotEncoder(`This is secret. Message about "_" symbol!`, 4) ===
     'Xlmw mw wigvix. Qiwweki efsyx "_" wcqfsp!'
 );
 console.log(
-  RotEncoder(`This is secret. Message about "_" symbol!`, 7) ===
+  rotEncoder(`This is secret. Message about "_" symbol!`, 7) ===
     'Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!'
 );
 console.log(
-  RotEncoder(`This is secret. Message about "_" symbol!`, 8) ===
+  rotEncoder(`This is secret. Message about "_" symbol!`, 8) ===
     'Bpqa qa amkzmb. Umaaiom ijwcb "_" agujwt!'
 );
 console.log(
-  RotEncoder(`This is secret. Message about "_" symbol!`, 13) ===
+  rotEncoder(`This is secret. Message about "_" symbol!`, 13) ===
     'Guvf vf frperg. Zrffntr nobhg "_" flzoby!'
 );
 
 console.log("decoding rot test");
 
 console.log(
-  RotEncoder(`This is secret. Message about "_" symbol!`, -1) ===
+  rotEncoder(`This is secret. Message about "_" symbol!`, -1) ===
     'Sghr hr rdbqds. Ldrrzfd zants "_" rxlank!'
 );
 console.log(
-  RotEncoder(`This is secret. Message about "_" symbol!`, -4) ===
+  rotEncoder(`This is secret. Message about "_" symbol!`, -4) ===
     'Pdeo eo oaynap. Iaoowca wxkqp "_" ouixkh!'
 );
 console.log(
-  RotEncoder(`This is secret. Message about "_" symbol!`, -7) ===
+  rotEncoder(`This is secret. Message about "_" symbol!`, -7) ===
     'Mabl bl lxvkxm. Fxlltzx tuhnm "_" lrfuhe!'
 );
 console.log(
-  RotEncoder(`This is secret. Message about "_" symbol!`, -8) ===
+  rotEncoder(`This is secret. Message about "_" symbol!`, -8) ===
     'Lzak ak kwujwl. Ewkksyw stgml "_" kqetgd!'
 );
 console.log(
-  RotEncoder(`This is secret. Message about "_" symbol!`, -13) ===
+  rotEncoder(`This is secret. Message about "_" symbol!`, -13) ===
     'Guvf vf frperg. Zrffntr nobhg "_" flzoby!'
+);
+
+console.log("Atbash test");
+
+console.log(
+  atbashEncoder(`This is secret. Message about "_" symbol!`) ===
+    `Gsrh rh hvxivg. Nvhhztv zylfg "_" hbnylo!`
+);
+console.log(
+  atbashEncoder(`is a monoalphabetic substitution cipher `) ===
+    `rh z nlmlzokszyvgrx hfyhgrgfgrlm xrksvi `
+);
+console.log(
+  atbashEncoder(`Articles that describe this calculator`) ===
+    `Zigrxovh gszg wvhxiryv gsrh xzoxfozgli`
+);
+console.log(
+  atbashEncoder(`by setting both "a" and "b" to 25. я уеду жить в Лондон`) ===
+    `yb hvggrmt ylgs "z" zmw "y" gl 25. я уеду жить в Лондон`
 );
 
 // console.log(process.stderr.write("что-то пошло не так"));
