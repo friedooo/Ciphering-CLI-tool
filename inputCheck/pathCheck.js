9def72d8b5c18f3941dd44e43318c2e43a683058
friedooo <friedlich3@gmail.com>
2021-11-11 20:17:24 +0300

    feat: add append output

--- inputCheck/pathCheck.js
+++ inputCheck/pathCheck.js
@@ -4,6 +4,6 @@ const { stdin, stdout, stderr } = process;
 
 module.exports = function checkPath(path, info) {
   return !fs.existsSync(path)
-    ? stderr.write(`некорректно указан путь ${info} \n`)
+    ? stderr.write(`некорректно указан путь до ${info} \n`)
     : true;
 };
